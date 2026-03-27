9c2c70cdbe46076e00355c630a5e7552fd831f15
yumi <yumisunky@gmail.com>
2026-03-27 20:19:52 +0800

    商品功能-查詢商品列表

--- src/main/java/com/yumi/springbootmall/dao/ProductDao.java
+++ src/main/java/com/yumi/springbootmall/dao/ProductDao.java
@@ -3,7 +3,11 @@ package com.yumi.springbootmall.dao;
 import com.yumi.springbootmall.dto.ProductRequest;
 import com.yumi.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+    List<Product> getProducts();
+
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
--- src/main/java/com/yumi/springbootmall/service/ProductService.java
+++ src/main/java/com/yumi/springbootmall/service/ProductService.java
@@ -3,7 +3,11 @@ package com.yumi.springbootmall.service;
 import com.yumi.springbootmall.dto.ProductRequest;
 import com.yumi.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+    List<Product> getProducts();
+
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
