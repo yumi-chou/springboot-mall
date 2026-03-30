50b4037de8f38078358e01f1baea3af72657f73b
yumi <yumisunky@gmail.com>
2026-03-30 11:53:21 +0800

    商品功能-商品列表分頁

--- src/main/java/com/yumi/springbootmall/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/yumi/springbootmall/dao/impl/ProductDaoImpl.java
@@ -33,6 +33,7 @@ public class ProductDaoImpl implements ProductDao {
 
         Map<String, Object> map = new HashMap<>();
 
+        // 查詢條件
         if (productQueryParams.getCategory() != null){
             sql = sql + " AND category = :category";
             map.put("category", productQueryParams.getCategory().name());
@@ -42,8 +43,13 @@ public class ProductDaoImpl implements ProductDao {
             sql = sql + " AND product_name LIKE :search";
             map.put("search", "%" + productQueryParams.getSearch() + "%");
         }
-
+        // 排序
         sql = sql + " ORDER BY " + productQueryParams.getOrderBy() + " " + productQueryParams.getSort();
+
+        // 分頁
+        sql = sql + " LIMIT :limit OFFSET :offset ";
+        map.put("limit", productQueryParams.getLimit());
+        map.put("offset", productQueryParams.getOffset());
 
         List<Product> productList = namedParameterJdbcTemplate.query(sql, map, new ProductRowMapper());
 
